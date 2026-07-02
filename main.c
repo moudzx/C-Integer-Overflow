abecad870ed321dc50b811261fb92099dcef6604
Mhamad Mostafa <141077164+moudzx@users.noreply.github.com>
2026-07-02 22:04:48 +0300

    Change include from 'intof.h' to 'ioverflow.h'

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-#include <intof.h>
+#include <ioverflow.h>
 #include <stdio.h>
 
 
 
 int main(void) {
-    // int8, int16, and int32 are typedefs for unsigned char, unsigned short, and unsigned int.
     // would_overflow()
     return 0;
 }
